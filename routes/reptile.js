1620aa115a63104d7517bf4141a15897e9fcbad6
Awei <13650928419@163.com>
2019-01-15 15:59:42 +0800

    factor: delete file.

--- routes/reptile.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import TaskControllers from '../controllers/task';
-const router = express.Router();
-
-
-router.get('/reptile',TaskControllers.reptileAllJob);
-
-export default router;
